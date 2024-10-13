f87ff638057e1fca624b29da3138357d90fdd173
demik245 <139484540+demik245@users.noreply.github.com>
2024-10-13 22:10:23 +0000

    gitignore

--- src/App.jsx
+++ src/App.jsx
@@ -1,29 +1,50 @@
 import './App.css';
+import { useState } from 'react';
 
 function App() {
+  const [result, setResult] = useState('');
+
+  function appendVal(val) {
+    setResult((prevResult) => prevResult + val);
+  }
+
+  function Clear() {
+    setResult('');
+  }
+
+  function Submit() {
+    try {
+      setResult(eval(result).toString());
+    } catch (e) {
+      setResult('Error');
+    }
+  }
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src="Octocat.png" className="App-logo" alt="logo" />
-        <p>
-          GitHub Codespaces <span className="heart">♥️</span> React
-        </p>
-        <p className="small">
-          Edit <code>src/App.jsx</code> and save to reload.
-        </p>
-        <p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </p>
-      </header>
+      <form>
+        <p className="result">{result}</p>
+        <div className="button-grid">
+          <input type="button" value="1" onClick={() => appendVal('1')} />
+          <input type="button" value="2" onClick={() => appendVal('2')} />
+          <input type="button" value="3" onClick={() => appendVal('3')} />
+          <input type="button" value="4" onClick={() => appendVal('4')} />
+          <input type="button" value="5" onClick={() => appendVal('5')} />
+          <input type="button" value="6" onClick={() => appendVal('6')} />
+          <input type="button" value="7" onClick={() => appendVal('7')} />
+          <input type="button" value="8" onClick={() => appendVal('8')} />
+          <input type="button" value="9" onClick={() => appendVal('9')} />
+          <input type="button" value="0" onClick={() => appendVal('0')} />
+          <input type="button" value="-" onClick={() => appendVal('-')} />
+          <input type="button" value="/" onClick={() => appendVal('/')} />
+          <input type="button" value="*" onClick={() => appendVal('*')} />
+          <input type="button" value="+" onClick={() => appendVal('+')} />
+          <input type="button" value="C" onClick={Clear} className="clear-button" />
+          <input type="button" value="S" onClick={Submit} className="submit-button" />
+        </div>
+      </form>
     </div>
   );
 }
 
-export default App;
+export default App
